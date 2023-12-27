ffaa6c872411d77c95df78ba8fe292e93e3fc20c
御风 <18012261618@126.com>
2023-12-27 18:11:58 +0800

    fix: $destination中this指向问题

--- packages/pc/src/plugins/router/index.js
+++ packages/pc/src/plugins/router/index.js
@@ -1,11 +1,11 @@
-import {  encodeUrl, downloadClick } from '@lcap/core-template';
+import { encodeUrl, downloadClick } from '@lcap/core-template';
 
-export function destination (url, target = '_self') {
+export function destination(url, target = '_self') {
     if (target === '_self') {
         // 修复访问路径为默认首页 / 时跳转可能失效的问题
-        if (url.startsWith('http'))
+        if (url.startsWith('http')) {
             location.href = encodeUrl(url);
-        else {
+        } else {
             // 处理同页面锚点跳转无效的问题
             const beforeHashUrl = url.slice(0, url.indexOf('#'));
             if (url.indexOf('#') !== -1 && beforeHashUrl.indexOf(location.pathname) !== -1) {
@@ -21,4 +21,4 @@ export function destination (url, target = '_self') {
     } else {
         downloadClick(url, target);
     }
-};
+}

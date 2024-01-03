ab5a5b8c8b30210ee4be1aa81661751a760b6daf
zhongzichen <zhongzichen@corp.netease.com>
2024-01-03 13:58:24 +0800

    补充h5缺失的默认值代码

--- packages/h5/src/plugins/dataTypes/index.js
+++ packages/h5/src/plugins/dataTypes/index.js
@@ -7,8 +7,12 @@ export function getFrontendVariables(options) {
 
     if (Array.isArray(options && options.frontendVariables)) {
         options.frontendVariables.forEach((frontendVariable) => {
-            const { name, typeAnnotation, defaultValue, localCache } = frontendVariable;
+            const { name, typeAnnotation, defaultValueFn, defaultCode, localCache } = frontendVariable;
             localCache && localCacheVariableSet.add(name); // 本地存储的全局变量集合
+            let defaultValue = defaultCode?.code;
+            if (Object.prototype.toString.call(defaultValueFn) === '[object Function]') {
+                defaultValue = defaultValueFn(Vue);
+            }
             frontendVariables[name] = genInitFromSchema(genSortedTypeKey(typeAnnotation), defaultValue);
         });
     }

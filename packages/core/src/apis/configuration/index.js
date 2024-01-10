941e729d2ae65158d378660157099ec0793a2a82
庞概 <panggai@corp.netease.com>
2024-01-10 11:27:44 +0800

    :fire: Rm useless code

--- packages/core/src/apis/configuration/index.js
+++ packages/core/src/apis/configuration/index.js
@@ -1,10 +1,7 @@
-import merge from 'lodash/merge';
-
 import { createService } from "../../utils/create";
-import apiConfig from './api.config';
 import api from './api';
 
-const initService = () => createService(merge(api, apiConfig));
+const initService = () => createService(api);
 
 export {
     initService,

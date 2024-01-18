472871e3aa054e8474d7a1b729617beaa82122d6
姜令浩 <jianglinghao@corp.netease.com>
2024-01-18 15:21:00 +0800

    feat: fix toast ui

--- packages/pc/src/init.js
+++ packages/pc/src/init.js
@@ -107,10 +107,13 @@ const init = (appConfig, platformConfig, routes, metaData) => {
             console.error(err);
         }
     };
-
+    if (!window?.$toast) {
+        window.$toast =  window.Vue.prototype.$toast;
+    }
     if (window?.rendered) {
         window.rendered();
     }
+
     const baseResourcePaths = platformConfig.baseResourcePaths || [];
     const authResourcePaths = platformConfig.authResourcePaths || [];
     const baseRoutes = filterRoutes(routes, null, (route, ancestorPaths) => {

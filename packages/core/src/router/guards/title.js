e0c8bb2e191b2ad662deaa326b66b2b0c0c16f45
shenpengyun <shenpengyun@corp.netease.com>
2024-02-01 17:04:35 +0800

    feat:修改路由拦截next的逻辑，和title修改逻辑

--- packages/core/src/router/guards/title.js
+++ packages/core/src/router/guards/title.js
@@ -1,14 +1,3 @@
-import Vue from 'vue';
-
 export const getComponentOption = function (routerItem) {
-    if (routerItem.components.default) {
-        const ctor = routerItem.components.default._Ctor;
-        let componentOptions;
-        if (ctor && ctor[0]) {
-            componentOptions = ctor[0].options;
-        } else {
-            componentOptions = Vue.extend(routerItem.components.default).options;
-        }
-        return componentOptions;
-    }
+    return routerItem || {};
 };

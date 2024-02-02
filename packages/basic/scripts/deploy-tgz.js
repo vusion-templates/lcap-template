73d402fbd50cc99179d54c7d61929f22357eee51
御风 <18012261618@126.com>
2024-02-02 16:42:48 +0800

    chore: add deploy script

---
+++ packages/basic/scripts/deploy-tgz.js
@@ -0,0 +1,13 @@
+const deployTgz = require('../../../scripts/deploy-tgz');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+
+const pkg = require('../package.json');
+const version = argv.version || pkg.version;
+
+deployTgz({
+  root: path.resolve(__dirname, "../"),
+  name: pkg.name,
+  version: version,
+  platform: argv.platform,
+});

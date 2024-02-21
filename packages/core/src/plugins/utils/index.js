e712267a025df1220e9d88343b84cff8f094d8f3
wangxiang <wangxiang08@corp.netease.com>
2024-02-21 17:44:54 +0800

    feat: 新增修改格式化函数

--- packages/core/src/plugins/utils/index.js
+++ packages/core/src/plugins/utils/index.js
@@ -1038,6 +1038,33 @@ export const utils = {
     }
     return dateFormatter.format(naslDateToLocalDate(value), formatter);
   },
+  FormatTime(value, formatter) {
+    if (!value) {
+      return "-";
+    }
+    // 使用正则表达式提取时、分、秒
+    const parts = value.match(/(\d{1,2})[^0-9]*(\d{1,2})[^0-9]*(\d{1,2})/);
+
+    // 如果没有匹配到三个部分，则返回原始字符串
+    if (!parts) {
+      return value;
+    }
+
+    // 提取时、分、秒，并将它们转换成整数
+    let hours = parseInt(parts[1], 10);
+    let minutes = parseInt(parts[2], 10);
+    let seconds = parseInt(parts[3], 10);
+
+    // 根据需要格式化时、分、秒
+    let formattedTime = formatter
+      .replace('HH', hours.toString().padStart(2, '0'))
+      .replace('H', hours.toString())
+      .replace('mm', minutes.toString().padStart(2, '0'))
+      .replace('m', minutes.toString())
+      .replace('ss', seconds.toString().padStart(2, '0'))
+      .replace('s', seconds.toString());
+    return formattedTime;
+  },
   FormatDateTime(value, formatter, tz) {
     if (!value) {
       return "-";
@@ -1151,14 +1178,20 @@ export const utils = {
   /**
    * 数字格式化
    * @param {digits} 小数点保留个数
+   * @param {omit} 是否隐藏末尾零
    * @param {showGroup} 是否显示千位分割（默认逗号分隔）
+   * @param {fix} 前缀还是后缀
+   * @param {unit} 单位
    */
-  FormatNumber(value, digits, showGroup) {
+  FormatNumber(value, digits, omit, showGroup, fix, unit) {
     if (!value) return value;
     if (parseFloat(value) === 0) return "0";
     if (isNaN(parseFloat(value)) || isNaN(parseInt(digits))) return;
     if (digits !== undefined) {
       value = Number(value).toFixed(parseInt(digits));
+      if (omit) {
+        value = parseFloat(value) + ''; // 转字符串
+      }
     }
     if (showGroup) {
       const temp = ("" + value).split(".");
@@ -1176,7 +1209,55 @@ export const utils = {
       if (right) left = left + "." + right;
       value = left;
     }
+    if (fix && unit) {
+      switch (fix) {
+        case "prefix":
+          value = unit + value;
+          break;
+        case "suffix":
+          value = value + unit;
+          break;
+        default:
+          value = value + unit;
+          break;
+      }
+    }
     return "" + value;
+  },
+  /**
+   * 百分数格式化
+   * @param {digits} 小数点保留个数
+   * @param {omit} 是否隐藏末尾零
+   * @param {showGroup} 是否显示千位分割（默认逗号分隔）
+   */
+  FormatPercent(value, digits, omit, showGroup) {
+    if (!value) return value;
+    if (parseFloat(value) === 0) return "0";
+    if (isNaN(parseFloat(value)) || isNaN(parseInt(digits))) return;
+    value = value * 100;
+    if (digits !== undefined) {
+      value = Number(value).toFixed(parseInt(digits));
+      if (omit) {
+        value = parseFloat(value) + ""; // 转字符串
+      }
+    }
+    if (showGroup) {
+      const temp = ("" + value).split(".");
+      const right = temp[1];
+      let left = temp[0]
+        .split("")
+        .reverse()
+        .join("")
+        .match(/(\d{1,3})/g)
+        .join(",")
+        .split("")
+        .reverse()
+        .join("");
+      if (temp[0][0] === "-") left = "-" + left;
+      if (right) left = left + "." + right;
+      value = left;
+    }
+    return value + "%";
   },
   /**
    * 时间差

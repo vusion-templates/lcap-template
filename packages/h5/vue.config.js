b280db179ddddbd3943118dc648e507527fb8be2
御风 <18012261618@126.com>
2024-03-20 19:30:25 +0800

    fix: 删除多余打包配置文件

--- packages/h5/vue.config.js
+++ packages/h5/vue.config.js
@@ -1,71 +1,50 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { EsbuildPlugin } = require('esbuild-loader');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const pkg = require('./package.json');
-const pages = require('./pages.json');
-const argv = require('minimist')(process.argv.slice(2));
-if (argv.pages) {
-    argv.pages = argv.pages.split(',');
-    Object.keys(pages).forEach((key) => {
-        if (!argv.pages.includes(key)) delete pages[key];
-    });
-}
+
 const isDevelopment = process.env.NODE_ENV === 'development';
-const publicPathPrefix = process.env.SITE_TYPE === 'gh-pages' ? `https://vusion-templates.github.io/${pkg.name}` : '/';
 
-const port = argv.port || 8810;
-
-const devServer = require('./webpack.dev-server')(port);
-
-const webpackDll = require('./webpack/dll');
-const webpackStyle = require('./webpack/style');
-const webpackDesigner = require('./webpack/designer');
-const webpackRoutes = require('./webpack/routes');
-const webpackHtml = require('./webpack/html');
 const webpackOptimization = require('./webpack/optimization');
-const isDesigner = process.env.BUILD_LIB_ENV === 'designer';
 
-const baseConfig = {
-    publicPath: publicPathPrefix,
+const vueConfig = {
+    publicPath: '/',
     outputDir: 'public',
     assetsDir: 'public',
     productionSourceMap: false,
-    transpileDependencies: [/lodash/, 'resize-detector', /cloud-ui\.vusion/, /@cloud-ui/, /vant/, /@lcap\/mobile-ui/],
-};
-const vueConfig = {
-    ...baseConfig,
-    pages,
+    transpileDependencies: [/lodash/, 'resize-detector'],
     chainWebpack(config) {
-        if (isDesigner) {
-            webpackDesigner.chain(config, pages);
-            config
-                .plugin('html')
-                .use(HtmlWebpackPlugin, [{}])
-                .tap((args) => {
-                    args[0].template = path.resolve('./demo.html');
-                    return args;
-                });
-        } else {
-            webpackHtml.chain(config, isDevelopment);
-            webpackDll.chain(config, publicPathPrefix, isDevelopment);
-        }
-        webpackOptimization.chain(config, isDevelopment);
-
-        webpackStyle.chain(config);
-        webpackRoutes.chain(config);
-        config.output.jsonpFunction('webpackJsonp' + pkg.name);
-
+        config.externals({
+            ...config.get('externals'),
+            '@lcap/mobile-ui': 'vant',
+        });
+        config.resolve.alias.set('cloud-ui.vusion.css$', path.resolve(__dirname, '../node_modules/cloud-ui.vusion/dist-raw/index.css'));
         config.module.rule('js').uses.delete('cache-loader');
+
+        config.plugin('copy-cloud-ui-chunks').use(CopyWebpackPlugin, [
+            [
+                {
+                    context: 'node_modules/cloud-ui.vusion/dist-raw',
+                    from: 'chunk-*',
+                    to: 'public/js/',
+                },
+            ],
+        ]);
+
+        webpackOptimization.chain(config, isDevelopment);
+        if (config.plugins.has('css-sprite-plugin')) {
+            config.plugin('css-sprite-plugin').tap(([opts]) => {
+                opts.filename = '[name].[hash:16].[ext]';
+                return [opts];
+            });
+        }
     },
     configureWebpack: (config) => {
-        if (isDesigner) {
-            webpackDesigner.config(config, pages);
-        }
-
+        config.output.libraryExport = 'default';
+        config.output.jsonpFunction = 'webpackJsonp' + pkg.name;
         // 使用esbuild压缩
         config.optimization.minimizer = [new EsbuildPlugin()];
     },
-    devServer,
 };
 
 module.exports = vueConfig;

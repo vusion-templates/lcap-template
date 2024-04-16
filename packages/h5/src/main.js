5f50673926c505514ede77db7fb2b6d5d1055653
御风 <18012261618@126.com>
2024-04-16 17:40:48 +0800

    fix: 修复H5丢失library的引入

--- packages/h5/src/main.js
+++ packages/h5/src/main.js
@@ -3,6 +3,7 @@ import { routes } from './router/routes';
 
 import metaData from './metaData.js';
 import platformConfig from './platform.config.json';
+import './library';
 import cloudAdminDesigner from './init';
 
 cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);

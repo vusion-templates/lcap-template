615f74ef122d6a1806714762e4aa2933e1a6f95b
御风 <18012261618@126.com>
2024-04-18 11:15:55 +0800

    fix: 补充配置参考链接

--- packages/pc/src/setConfig.js
+++ packages/pc/src/setConfig.js
@@ -30,17 +30,19 @@ setConfig({
     },
 
     configureRequest(options) {
+        /**
+         * options配置参考
+         * https://axios-http.com/zh/docs/req_config
+         */
+
         // 修改请求baseURL
         // options.baseURL = 'https://some-domain.com/api';
-
         // 增加额外的请求头
         // options.headers['key1'] = 'value1';
         // options.headers['key2'] = 'value2';
-
         // 增加额外的请求参数（get请求）
         // options.params['key1'] = 'value1';
         // options.params['key2'] = 'value2';
-
         // 增加额外的请求参数（post请求）
         // options.data['key1'] = 'value1';
         // options.data['key2'] = 'value2';

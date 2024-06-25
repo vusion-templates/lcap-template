57b7156245feaa67482a3e37bb8e1c8340ede910
御风 <18012261618@126.com>
2024-06-25 16:09:25 +0800

    fix: define process

--- packages/h5/build/webpack.config.js
+++ packages/h5/build/webpack.config.js
@@ -80,6 +80,11 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
         new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
             filename: `${library}.css`,
--- packages/pc/build/webpack.config.js
+++ packages/pc/build/webpack.config.js
@@ -79,6 +79,11 @@ module.exports = {
         ],
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
         new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
             filename: `${library}.css`,

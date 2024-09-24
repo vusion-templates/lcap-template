0522a4bee7454b612e67007b470a51bde75e2140
御风 <18012261618@126.com>
2024-09-24 15:49:29 +0800

    fix: 调整main.js中library.js的引入顺序

--- packages/pc/src/main.js
+++ packages/pc/src/main.js
@@ -2,7 +2,7 @@
 import metaData from './metaData.js';
 import platformConfig from './platform.config.json';
 import { routes } from './router/routes';
-import './library';
 import cloudAdminDesigner from './init';
+import './library';
 
 cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);

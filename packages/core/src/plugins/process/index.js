580ea049522073e5b76b839a37ba2178c517b437
御风 <18012261618@126.com>
2024-09-29 11:38:51 +0800

    feat: processV2接口迁移到systemProvessV2

--- packages/core/src/plugins/process/index.js
+++ packages/core/src/plugins/process/index.js
@@ -1,5 +1,4 @@
 import processService from './processService';
-import { initService as initProcessV2Service } from "../../apis/processV2";
 import { initService as initSystemProcessV2Service } from "../../apis/system/processV2";
 
 export default {
@@ -8,7 +7,7 @@ export default {
          * 流程接口注册
          */
         Vue.prototype.$process = processService;
-        Vue.prototype.$processV2 = initProcessV2Service();
+        Vue.prototype.$processV2 = initSystemProcessV2Service();
         Vue.prototype.$systemProcessV2 = initSystemProcessV2Service();
     },
 };
